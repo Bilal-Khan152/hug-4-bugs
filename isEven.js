efba517ba801d9ce9a7dd2e89552515a1473c231
Fazal Sultan <p180114@nu.edu.pk>
2025-10-24 00:41:11 +0500

    is even or odd

---
+++ isEven.js
@@ -0,0 +1,6 @@
+/*
+    Write a JS script which can identify that is the given number is even or not using ternory operator
+ */
+ let Num = 50;
+    let isEven =  Num % 2 === 0 ? 'Even' : 'Odd'
+    console.log('The Given number Is: ' , isEven)
